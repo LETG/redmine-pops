236ce4d74589e34e8f3911174edfa7cc6d449729
Alban Merino <amerino@dotgee.fr>
2014-02-27 16:37:44 +0100

    puts the part with dmsf in another plugin

--- lib/pops_project_hook_listener.rb
+++ lib/pops_project_hook_listener.rb
@@ -1,5 +1,4 @@
 class PopsProjectHookListener < Redmine::Hook::ViewListener
   render_on :view_projects_form, partial: 'projects/pops_form'
   render_on :view_projects_show_left, partial: 'projects/pops_show_left'
-  render_on :view_projects_show_right, partial: 'projects/pops_show_right'
 end
