514f643d3c798fca2e4432ee2b4373d608c5cb95
Alban Merino <amerino@dotgee.fr>
2014-02-27 16:05:49 +0100

    add view for docs

--- lib/pops_project_hook_listener.rb
+++ lib/pops_project_hook_listener.rb
@@ -1,4 +1,5 @@
 class PopsProjectHookListener < Redmine::Hook::ViewListener
   render_on :view_projects_form, partial: 'projects/pops_form'
-  render_on :view_projects_show_left, partial: 'projects/pops_show'
+  render_on :view_projects_show_left, partial: 'projects/pops_show_left'
+  render_on :view_projects_show_right, partial: 'projects/pops_show_right'
 end
