7ae485cd903adaeae574c9a6383a54dbdf0d2e9d
Alban Merino <amerino@dotgee.fr>
2014-03-26 10:26:21 +0100

    Résumé (#2850)
    Change affichage de l'aperçu pour avoir résumé en dessous du titre du projet.
    Le résumé devient champ obligatoire.
    Les objectifs ne sont plus obligatoire.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ module PopsProject
   extend ActiveSupport::Concern
   included do
     safe_attributes 'objectifs', 'accronym', 'resume', 'methodologies', 'results', 'perspectives', 'comments', 'url_public', 'url_project', 'start_date', 'support_id'
-    validates :accronym, :objectifs, presence: true, allow_nil: false
+    validates :accronym, :resume, presence: true, allow_nil: false
     belongs_to :support
   end
 end
