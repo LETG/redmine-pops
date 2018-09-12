94bdb0b9488b3241f5a34cf5046435c1cef89021
Julien ROGER <julien.roger@net-helium.fr>
2018-09-12 16:50:31 +0200

    correction bloc dernieres annonces

--- app/controllers/projects_controller_patch.rb
+++ app/controllers/projects_controller_patch.rb
@@ -58,6 +58,34 @@ module ProjectsControllerPatch
           }
         end
       end
+
+      def show
+        # try to redirect to the requested menu item
+        if params[:jump] && redirect_to_project_menu_item(@project, params[:jump])
+          return
+        end
+
+        @users_by_role = @project.users_by_role
+        @subprojects = @project.children.visible.to_a
+        @news = @project.news.visible.limit(5).includes(:author, :project).order("#{News.table_name}.created_on DESC").to_a
+        @trackers = @project.rolled_up_trackers.visible
+
+        cond = @project.project_condition(Setting.display_subprojects_issues?)
+
+        @open_issues_by_tracker = Issue.visible.open.where(cond).group(:tracker).count
+        @total_issues_by_tracker = Issue.visible.where(cond).group(:tracker).count
+
+        if User.current.allowed_to_view_all_time_entries?(@project)
+          @total_hours = TimeEntry.visible.where(cond).sum(:hours).to_f
+        end
+
+        @key = User.current.rss_key
+
+        respond_to do |format|
+          format.html
+          format.api
+        end
+      end
     end
   end
 end
