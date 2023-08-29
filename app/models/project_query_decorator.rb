3fdb2259876649e8d564ef78ef5d8e2314907a49
Julien ROGER <julien.roger@net-helium.fr>
2023-08-29 15:55:03 +0200

    ajout du filtrage sur les projets

--- app/models/project_query_decorator.rb
+++ app/models/project_query_decorator.rb
@@ -1,14 +1,17 @@
 ProjectQuery.class_eval do
   def initialize(attributes=nil, *args)
     super attributes
-    self.filters ||= {}
+    self.filters ||= {} # { 'status' => { operator: "=", values: ['1'] } }
   end
 
   def initialize_available_filters
-    add_available_filter("labs",         type: :list, values: lambda {lab_values},                               label: :field_lab_partner)
-    add_available_filter("coordinators", type: :list, values: lambda {member_values('Coordinateur')},            label: :field_coordinators)
-    add_available_filter("participants", type: :list, values: lambda {member_values},                            label: :field_participants)
-    add_available_filter("support_id",   type: :list, values: Support.all.collect { |s| [ s.name, s.id.to_s ] }, label: :field_support)
+    add_available_filter("labs",         type: :list,    values: lambda {lab_values},                               label: :field_lab_partner)
+    add_available_filter("coordinators", type: :list,    values: lambda {member_values('Coordinateur')},            label: :field_coordinators)
+    add_available_filter("participants", type: :list,    values: lambda {member_values},                            label: :field_participants)
+    add_available_filter("support_id",   type: :list,    values: Support.all.collect { |s| [ s.name, s.id.to_s ] }, label: :field_support)
+    add_available_filter("year",         type: :list,    values: year_values,                                       label: :field_year)
+    add_available_filter("status",       type: :list,    values: lambda {project_statuses_values}
+    )
   end
 
   def lab_values
@@ -32,9 +35,19 @@ ProjectQuery.class_eval do
       end
 
       h
-    end.sort.to_h
+    end.sort_by { |_key, value| value[:name] }.to_h
 
     users.collect { |k,v| [ v[:name], v[:ids].join('|') ] }
+  end
+
+  def year_values
+    available_years = Project.all.inject([]) do |arr, p|
+      arr << p.starts_date.year if p.starts_date && !arr.include?(p.starts_date.year) && p.starts_date.year > 2000 && p.starts_date.year < 3000
+      arr << p.ends_date.year   if p.ends_date && !arr.include?(p.ends_date.year) && p.ends_date.year > 2000 && p.ends_date.year < 3000
+      arr
+    end.sort
+
+    (available_years.min..available_years.max).map { |i| [ i.to_s, i.to_s ] }
   end
 
   def sql_for_labs_field(field, operator, value)
@@ -53,6 +66,19 @@ ProjectQuery.class_eval do
   def sql_for_members_field(field, operator, value)
     value = value.map { |v| v.split('|') }.flatten
     sql_for_field(field, operator, value, Member.table_name, "id")
+  end
+
+  def sql_for_year_field(field, operator, value)
+    neg   = (operator == '!' ? 'NOT' : '')
+
+    query = value.inject([]) do |arr, val|
+      arr << "(#{Project.table_name}.starts_date < '#{Date.new((val.to_i + 1), 1, 1)}' AND #{Project.table_name}.ends_date >= '#{Date.new(val.to_i, 1, 1)}')"
+      arr << "(#{Project.table_name}.starts_date >= '#{Date.new(val.to_i, 1, 1)}' AND #{Project.table_name}.starts_date < '#{Date.new((val.to_i + 1), 1, 1)}' AND #{Project.table_name}.ends_date IS NULL)"
+      arr
+    end
+
+    subquery = "SELECT id FROM #{Project.table_name} WHERE #{query.join(' OR ')}"
+    "#{neg} #{Project.table_name}.id IN (#{subquery})"
   end
 
   def results_scope(options = {})
